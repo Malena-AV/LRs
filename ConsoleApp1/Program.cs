3a98c6e3d282089eded10175ecd17e25fb43c29f
argentoff1 <mparinos56@gmail.com>
2022-01-19 13:20:35 +0300

    корректировка классов, создание библиотеки

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mal = new MallardDuck();
+            Duck red = new RedheadDuck();
+
+            Duck[] duck = new Duck[] { mal, red };
+
+            for (int i = 0; i < duck.Length; i++)
+            {
+                Console.WriteLine(duck[i].Quack());
+                Console.WriteLine(duck[i].Swim());
+                Console.WriteLine(duck[i].DisPlay());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Library/MallardDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MallardDuck : Duck
+    {
+        public override string DisPlay()
+        {
+            return "DisPlay" + " " + this.GetType();
+        }
+    }
+}
